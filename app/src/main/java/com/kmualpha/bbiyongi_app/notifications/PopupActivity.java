959e5e1ffc1e185c4b5b080fce31205f3a9f1728
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-04-30 08:53:51 +0900

    ✨feat: Popup Activity 뷰 객체 생성

--- app/src/main/java/com/kmualpha/bbiyongi_app/notifications/PopupActivity.java
+++ app/src/main/java/com/kmualpha/bbiyongi_app/notifications/PopupActivity.java
@@ -1,4 +1,31 @@
 package com.kmualpha.bbiyongi_app.notifications;
 
-public class PopupActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.kmualpha.bbiyongi_app.R;
+
+public class PopupActivity extends Activity {
+
+    EditText edit_msg;
+    TextView btn_set_return;
+    TextView btn_get_pos;
+    TextView btn_get_date;
+    TextView btn_get_link;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_setting);
+
+        edit_msg = findViewById(R.id.edit_msg);
+
+        btn_set_return = findViewById(R.id.btn_set_return);
+
+    }
 }
