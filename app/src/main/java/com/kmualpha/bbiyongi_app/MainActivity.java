03fe3c71751dcc070ea161e42bd26acb48538810
Lee Sehyeon <84698896+sehyeon518@users.noreply.github.com>
2023-04-30 10:05:08 +0900

    ✨feat: 메인 화면에서 알림 내역 화면으로 전환 기능

--- app/src/main/java/com/kmualpha/bbiyongi_app/MainActivity.java
+++ app/src/main/java/com/kmualpha/bbiyongi_app/MainActivity.java
@@ -2,13 +2,54 @@ package com.kmualpha.bbiyongi_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.kmualpha.bbiyongi_app.notifications.NotificationsActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String[] permissionList = new String[] {Manifest.permission.SEND_SMS};
+
+    TextView btn_notifications;
+    ImageView btn_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        btn_notifications = findViewById(R.id.btn_notifications);
+        btn_notifications.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
+            startActivity(intent);
+        });
+
+        btn_setting = findViewById(R.id.setting);
+        btn_setting.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), PopupActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (String permission : permissionList) {
+                if (checkCallingOrSelfPermission(permission) == PackageManager.PERMISSION_DENIED)
+                    requestPermissions(permissionList, 0);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
